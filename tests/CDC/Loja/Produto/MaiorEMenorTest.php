8951a8aa3cfec25b124e9a5595d75b29758f78ec
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2024-12-02 12:48:51 -0300

    2.4 continuando a testar

--- tests/CDC/Loja/Produto/MaiorEMenorTest.php
+++ tests/CDC/Loja/Produto/MaiorEMenorTest.php
@@ -18,9 +18,17 @@ class MaiorEMenorTest extends PHPUnit
         $carrinho->adiciona(new Produto("Jogo de pratos", 70.00));
         $maiorMenor = new MaiorEMenor();
         $maiorMenor->encontra($carrinho);
-        $this->assertEquals("Jogo de pratos",
-            $maiorMenor->getMenor()->getNome());
-        $this->assertEquals("Geladeira",
-            $maiorMenor->getMaior()->getNome());
+        $this->assertEquals("Jogo de pratos", $maiorMenor->getMenor()->getNome());
+        $this->assertEquals("Geladeira", $maiorMenor->getMaior()->getNome());
+    }
+
+    public function testApenasUmProduto()
+    {
+        $carrinho = new CarrinhoDeCompras();
+        $carrinho->adiciona(new Produto("Geladeira", 450.00));
+        $maiorEMenor = new MaiorEMenor();
+        $maiorEMenor->encontra($carrinho);
+        $this->assertEquals("Geladeira", $maiorEMenor->getMenor()->getNome());
+        $this->assertEquals("Geladeira", $maiorEMenor->getMaior()->getNome());
     }
 }
