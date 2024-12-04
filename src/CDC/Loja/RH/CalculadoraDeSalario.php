8f1f21c033ef431334306ff4744dc9b189ac7b7e
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2024-12-04 09:25:49 -0300

    4.2 Implementando da maneira mais simples possível

---
+++ src/CDC/Loja/RH/CalculadoraDeSalario.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CDC\Loja\RH;
+
+class CalculadoraDeSalario
+{
+    public function calculaSalario(Funcionario $funcionario)
+    {
+        if ( $funcionario->getCargo()
+            === TabelaCargos::DESENVOLVEDOR) {
+            if ( $funcionario->getSalario() > 3000 ) {
+                return 3200.0;
+            }
+            return 1350.0;
+        }
+        return 425.0;
+    }
+}
---
+++ tests/CDC/RH/CalculadoraDeSalarioTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace CDC\RH;
+
+use PHPUnit\Framework\TestCase;
+use CDC\Loja\RH\Funcionario;
+use CDC\Loja\RH\TabelaCargos;
+use CDC\Loja\RH\CalculadoraDeSalario;
+
+class CalculadoraDeSalarioTest extends TestCase
+{
+
+    public function testCalculoSalarioDesenvolvedoresComSalarioAbaixoDoLimite()
+    {
+        $calculadora = new CalculadoraDeSalario();
+
+        $desenvolvedor = new Funcionario("Andre", 1500.0, TabelaCargos::DESENVOLVEDOR);
+
+        $salario = $calculadora->calculaSalario($desenvolvedor);
+
+        $this->assertEquals(1500.0 * 0.9, $salario, null, 0.00001);
+    }
+
+    public function testCalculoSalarioDesenvolvedoresComSalarioAcimaDoLimite()
+    {
+        $calculadora = new CalculadoraDeSalario();
+
+        $desenvolvedor = new Funcionario("Andre", 4000.0, TabelaCargos::DESENVOLVEDOR);
+
+        $salario = $calculadora->calculaSalario($desenvolvedor);
+
+        $this->assertEquals(4000.0 * 0.8, $salario, null, 0.00001);
+    }
+
+    public function testDeveCalcularSalarioParaDBAsComSalarioAbaixoDoLimite()
+    {
+        $calculadora = new CalculadoraDeSalario();
+
+        $dba = new Funcionario("Andre", 500.0, TabelaCargos::DBA);
+
+        $salario = $calculadora->calculaSalario($dba);
+
+        $this->assertEquals(500.0 * 0.85, $salario, null, 0.00001);
+    }
+}
